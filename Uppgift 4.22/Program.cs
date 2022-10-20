6558d2e4d2b2e2ca771ca25235b64006f6ad5bb1
Pingvu <nhlpool27@gmail.com>
2022-10-20 13:35:38 +0200

    Add project files.

---
+++ Uppgift 4.22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Uppgift_4_22
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 200;
+            int z = 1000;
+            while (z < 1400)
+            {
+                while (x <= 30)
+                {
+                    Console.WriteLine(x);
+                    x++;
+                }
+                
+                while (y >=180)
+                {
+                    
+                    Console.WriteLine(y);
+                    y--;
+                }
+                while (z <= 1400)
+                {
+                    Console.WriteLine(z);
+                    z = z + 50;
+                }
+            }
+        }
+    }
+}
